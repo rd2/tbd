aed2118732f38fdffbfd2dd3364d19a6a4acd545
XXX <dan.macumber@gmail.com>
2020-09-11 21:01:42 -0600

    Use a material we can compute uFactor for

--- lib/measures/tbd/resources/version.rb
+++ lib/measures/tbd/resources/version.rb
@@ -1,3 +1,3 @@
-module Topolys
-  VERSION = "0.1.0"
+module TBD
+  VERSION = "0.1.0".freeze
 end
--- spec/derated_construction_spec.rb
+++ spec/derated_construction_spec.rb
@@ -7,20 +7,21 @@ RSpec.describe TBD do
     os_model = OpenStudio::Model::Model.new
     space = OpenStudio::Model::Space.new(os_model)
 
-    soffit = OpenStudio::Model::StandardOpaqueMaterial.new(os_model)
-    soffit.setRoughness("Smooth")
-    soffit.setThickness(0.0159)
-    soffit.setConductivity(0.12)
-    soffit.setDensity(544)
-    soffit.setSpecificHeat(1210)
-    soffit.setThermalAbsorptance(0.9)
-    soffit.setSolarAbsorptance(0.7)
-    soffit.setVisibleAbsorptance(0.7)
+    material = OpenStudio::Model::MasslessOpaqueMaterial.new(os_model)
+    material.setRoughness("Smooth")
+    material.setThermalResistance(4.0)
+    material.setThermalAbsorptance(0.9)
+    material.setSolarAbsorptance(0.7)
+    material.setVisibleAbsorptance(0.7)
 
     layers = OpenStudio::Model::MaterialVector.new
-    layers << soffit
+    layers << material
     construction = OpenStudio::Model::Construction.new(os_model)
     construction.setLayers(layers)
+    expect(construction.thermalConductance.empty?).to be(false)
+    expect(construction.thermalConductance.get).to be_within(0.001).of(0.25)
+    expect(construction.uFactor(0).empty?).to be(false)
+    expect(construction.uFactor(0).get).to be_within(0.001).of(0.25)
 
     vertices = OpenStudio::Point3dVector.new
     vertices << OpenStudio::Point3d.new( 10, 10, 5)
@@ -33,6 +34,8 @@ RSpec.describe TBD do
     expect(ceiling.surfaceType.downcase).to eq("roofceiling")
     expect(ceiling.outsideBoundaryCondition.downcase).to eq("outdoors")
     expect(ceiling.filmResistance).to be_within(0.001).of(0.136)
+    expect(ceiling.uFactor.empty?).to be(false)
+    expect(ceiling.uFactor.get).to be_within(0.001).of(0.242)
 
     vertices = OpenStudio::Point3dVector.new
     vertices << OpenStudio::Point3d.new( 0, 10, 5)
@@ -46,6 +49,8 @@ RSpec.describe TBD do
     expect(wall.outsideBoundaryCondition.downcase).to eq("outdoors")
     expect(wall.tilt).to be_within(0.001).of(Math::PI/2.0)
     expect(wall.filmResistance).to be_within(0.001).of(0.150)
+    expect(wall.uFactor.empty?).to be(false)
+    expect(wall.uFactor.get).to be_within(0.001).of(0.241)
 
     vertices = OpenStudio::Point3dVector.new
     vertices << OpenStudio::Point3d.new( 0, 10, 0)
@@ -59,20 +64,29 @@ RSpec.describe TBD do
     expect(floor.outsideBoundaryCondition.downcase).to eq("ground")
     expect(floor.tilt).to be_within(0.001).of(Math::PI)
     expect(floor.filmResistance).to be_within(0.001).of(0.160)
+    expect(floor.uFactor.empty?).to be(false)
+    expect(floor.uFactor.get).to be_within(0.001).of(0.241)
 
     # make ground
     expect(floor.setOutsideBoundaryCondition("Outdoors")).to be(true)
     expect(floor.filmResistance).to be_within(0.001).of(0.190)
+    expect(floor.uFactor.empty?).to be(false)
+    expect(floor.uFactor.get).to be_within(0.001).of(0.239)
 
     # now make these surfaces not outdoors
     expect(ceiling.setOutsideBoundaryCondition("Adiabatic")).to be(true)
     expect(ceiling.filmResistance).to be_within(0.001).of(0.212)
+    expect(ceiling.uFactor.empty?).to be(false)
+    expect(ceiling.uFactor.get).to be_within(0.001).of(0.237)
 
     expect(wall.setOutsideBoundaryCondition("Adiabatic")).to be(true)
     expect(wall.filmResistance).to be_within(0.001).of(0.239)
+    expect(wall.uFactor.empty?).to be(false)
+    expect(wall.uFactor.get).to be_within(0.001).of(0.236)
 
     expect(floor.setOutsideBoundaryCondition("Adiabatic")).to be(true)
     expect(floor.filmResistance).to be_within(0.001).of(0.321)
-
+    expect(floor.uFactor.empty?).to be(false)
+    expect(floor.uFactor.get).to be_within(0.001).of(0.231)
   end
 end
