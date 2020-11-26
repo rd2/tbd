c48a8c8674cc43f2414bb10fbcd3caac3a2bfca1
XXX <dan.macumber@gmail.com>
2020-11-25 17:40:26 -0700

    Updates for measure

--- lib/measures/tbd/README.md
+++ lib/measures/tbd/README.md
@@ -19,10 +19,26 @@ ModelMeasure
 ## Arguments
 
 
-### Thermal bridge option
+### Load TBD.json
+Loads existing TDB.json from model directory, overrides other arguments if true.
+**Name:** load_tbd_json,
+**Type:** Boolean,
+**Units:** ,
+**Required:** true,
+**Model Dependent:** false
+
+### Default thermal bridge option to use if not reading TDB.json
 e.g. poor, regular, efficient, code
 **Name:** option,
 **Type:** Choice,
+**Units:** ,
+**Required:** true,
+**Model Dependent:** false
+
+### Write TBD.json
+Write TBD.json to customize for subsequent runs, edit and place in model directory
+**Name:** write_tbd_json,
+**Type:** Boolean,
 **Units:** ,
 **Required:** true,
 **Model Dependent:** false
--- lib/measures/tbd/measure.rb
+++ lib/measures/tbd/measure.rb
@@ -32,15 +32,27 @@ class TBDMeasure < OpenStudio::Measure::ModelMeasure
   # define the arguments that the user will input
   def arguments(model)
     args = OpenStudio::Measure::OSArgumentVector.new
-    psi = PSI.new
+
+    load_tbd_json = OpenStudio::Measure::OSArgument.makeBoolArgument("load_tbd_json", true, false)
+    load_tbd_json.setDisplayName("Load TBD.json")
+    load_tbd_json.setDescription("Loads existing TDB.json from model directory, overrides other arguments if true.")
+    load_tbd_json.setDefaultValue(false)
+    args << load_tbd_json
 
     choices = OpenStudio::StringVector.new
+    psi = PSI.new
     psi.set.keys.each do |k| choices << k.to_s; end
     option = OpenStudio::Measure::OSArgument.makeChoiceArgument("option", choices, true)
-    option.setDisplayName("Thermal bridge option")
+    option.setDisplayName("Default thermal bridge option to use if not reading TDB.json")
     option.setDescription("e.g. poor, regular, efficient, code")
     option.setDefaultValue("poor (BC Hydro)")
     args << option
+
+    write_tbd_json = OpenStudio::Measure::OSArgument.makeBoolArgument("write_tbd_json", true, false)
+    write_tbd_json.setDisplayName("Write TBD.json")
+    write_tbd_json.setDescription("Write TBD.json to customize for subsequent runs, edit and place in model directory")
+    write_tbd_json.setDefaultValue(true)
+    args << write_tbd_json
 
     return args
   end
@@ -50,9 +62,13 @@ class TBDMeasure < OpenStudio::Measure::ModelMeasure
     super(model, runner, user_arguments)
 
     # assign the user inputs to variables
+    load_tbd_json = runner.getBoolArgumentValue("load_tbd_json", user_arguments)
+
     option = runner.getStringArgumentValue("option", user_arguments)
     psi = PSI.new
     set = psi.set[option]
+
+    write_tbd_json = runner.getBoolArgumentValue("write_tbd_json", user_arguments)
 
     # use the built-in error checking
     if !runner.validateUserArguments(arguments(model), user_arguments)
@@ -66,6 +82,12 @@ class TBDMeasure < OpenStudio::Measure::ModelMeasure
         name   = id.rjust(15, " ")
         output = "#{name} RSi derated by #{ratio}%"
         runner.registerInfo(output)
+      end
+    end
+
+    if write_tbd_json
+      File.open('tbd.json', 'w') do |f|
+        f.puts JSON::pretty_generate(surfaces)
       end
     end
 
